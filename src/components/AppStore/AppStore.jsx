a25f3734386dd9e76f80739e6f8843e850e232dc
Kashif Ali <malikkashif5035@gmail.com>
2025-01-04 17:08:11 +0500

    Fixed file name from Play to play

--- src/components/AppStore/AppStore.jsx
+++ src/components/AppStore/AppStore.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import AppStoreImg from "../../assets/app_store.png";
-import PlayStoreImg from "../../assets/Play_store.png";
+import PlayStoreImg from "../../assets/play_store.png";
 import Gif from "../../assets/mobile_bike.gif";
 
 const AppStore = () => {
